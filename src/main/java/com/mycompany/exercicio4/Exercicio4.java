5b6d083f61bd3f0656dc05fa2ec584205e261f16
daros <daros@EsterMariani>
2025-04-08 18:27:23 -0300

    Inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio4/Exercicio4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio4;
+
+import javax.swing.JOptionPane;
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+       
+        String nome;
+        nome = "João";
+        JOptionPane.showMessageDialog(null, nome);
+        
+        nome = "Maria";
+        JOptionPane.showMessageDialog(null, "O valor da variável é: " + nome);
+        
+    }
+}
